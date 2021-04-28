d690698ef5c0315e91ea778a3cc40efd7019122b
36654 <wuchunquan@longshine.com>
2021-04-28 17:23:02 +0800

    -

--- src/main/java/com/x/bridge/web/controller/LoginController.java
+++ src/main/java/com/x/bridge/web/controller/LoginController.java
@@ -1,8 +1,14 @@
 package com.x.bridge.web.controller;
 
+import lombok.Data;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
 
 /**
  * @Desc
@@ -12,9 +18,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("login")
 public class LoginController {
-    
+
     @PostMapping
-    public String login(){
-        return "home";
+    public String login() {
+        return "admin/index";
     }
+
+    @GetMapping("hello")
+    public @ResponseBody Mono<A> hello() {
+        A ad = new A("AD", 1, LocalDateTime.now());
+        return Mono.just(ad);
+    }
+
+    @Data
+    private class A {
+
+        private String name;
+
+        private int age;
+
+        private LocalDateTime birthday;
+
+        public A(String name, int age, LocalDateTime birthday) {
+            this.name = name;
+            this.age = age;
+            this.birthday = birthday;
+        }
+
+    }
+
 }
